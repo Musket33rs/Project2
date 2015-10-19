f1087e5b3b9a6c6b114549c6c67c63f7236381f1
Jaime Martinez <jaime.martinez@clinicloud.com>
2015-10-19 22:38:56 +1100

    restructure

--- prolog/pman.pl
+++ prolog/pman.pl
@@ -3,9 +3,9 @@
 %init
 pmanAt(1,1,s0).
 
-%connected(X1,Y1,X2,Y2):- \+isWall(X2,Y2),
-%    (Y2 is Y1+1;Y2 is Y1-1),(X1 = X2);
-%    (X2 is X1+1;X2 is X1-1),(Y1 = Y2).
+connected(X1,Y1,X2,Y2):- \+isWall(X2,Y2),
+    (Y2 is Y1+1;Y2 is Y1-1),(X1 = X2);
+    (X2 is X1+1;X2 is X1-1),(Y1 = Y2).
 
 %connected(1,2,1,1,s0).
 isFree(1,2,s0).
@@ -15,61 +15,34 @@ isWall(0,0).
 
 %action moveRight
 %preconditions
-poss(moveRight,S):- pmanAt(X,Y,S),isFree(X,Y1,S),
+poss(moveRight,S):- pmanAt(X,Y,S),isFree(X,Y1,S), connected(X,Y,X,Y1),
                   (Y1 is Y+1),
                   \+ghostAt(X,Y1,S).
+poss(moveLeft,S):- pmanAt(X,Y,S),isFree(X,Y1,S), connected(X,Y,X,Y1),
+                  (Y1 is Y-1),
+                  \+ghostAt(X,Y1,S).
+poss(moveUp,S):- pmanAt(X,Y,S),isFree(X1,Y,S), connected(X,Y,X1,Y),
+                  (X1 is X+1),
+                  \+ghostAt(X1,Y,S).
+poss(moveDown,S):- pmanAt(X,Y,S),isFree(X1,Y,S), connected(X,Y,X1,Y),
+                  (X1 is X-1),
+                  \+ghostAt(X1,Y,S).
 %success state axioms moveRight
-pmanAt(X,Y,do(A,S)):-pmanAt(X,Y1,S),
+pmanAt(X,Y,do(A,S)):-pmanAt(X1,Y1,S),
       (
-        (A=moveRight,(Y is Y1+1))
+        (A=moveRight,(Y is Y1+1),(X1=X));
+        (A=moveLeft,(Y is Y1-1),(X1=X));
+        (A=moveUp,(X is X1+1),(Y1=Y));
+        (A=moveDown,(X is X1-1),(Y1=Y))
       );
       (
         pmanAt(X,Y,S),
-        \+ A=moveRight
-      ).
-%action moveLeft
-%preconditions
-poss(moveLeft,S):- pmanAt(X,Y,S),isFree(X,Y1,S),
-        (Y1 is Y-1),
-        \+ghostAt(X,Y1,S).
-%success state axioms moveRight
-pmanAt(X,Y,do(A,S)):-pmanAt(X,Y1,S),
-    (
-      (A=moveLeft,(Y is Y1-1))
-    );
-    (
-        pmanAt(X,Y,S),
-        \+ A=moveLeft
+        \+ A=moveRight;
+        \+ A=moveLeft;
+        \+ A=moveUp;
+        \+ A=moveDown
       ).
 
-  %action moveUp
-  %preconditions
-  poss(moveUp,S):- pmanAt(X,Y,S),isFree(X1,Y,S),
-                    (X1 is X+1),
-                    \+ghostAt(X1,Y,S).
-  %success state axioms moveUp
-  pmanAt(X,Y,do(A,S)):-pmanAt(X1,Y,S),
-        (
-          (A=moveRight,(X is X1+1))
-        );
-        (
-          pmanAt(X,Y,S),
-          \+ A=moveUp
-        ).
-  %action moveDown
-  %preconditions
-  poss(moveDown,S):- pmanAt(X,Y,S),isFree(X1,Y,S),
-          (X1 is X-1),
-          \+ghostAt(X1,Y,S).
-  %success state axioms moveRight
-  pmanAt(X,Y,do(A,S)):-pmanAt(X1,Y,S),
-      (
-        (A=moveDown,(X is X1-1))
-      );
-      (
-          pmanAt(X,Y,S),
-          \+ A=moveDown
-        ).
 
 
 /*legal axioms*/
